7a34b2ad49c71097b8155b05e1c5db1c275a55d3
Kanlaya <film_kanlaya@hotmail.com>
2021-02-08 02:26:00 +0700

    library-management

---
+++ library_management/library_management/doctype/library_member/library_member.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, it011 and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+class LibraryMember(WebsiteGenerator):
+	def before_save(self):
+		self.full_name = f'{self.first_name} {self.last_name or ""}'
+	
+	def after_insert(self):
+		frappe.msgprint (
+			msg = 'Successfully creat a Library Member',
+			title = 'Nice'
+		)
+
